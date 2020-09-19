2e19f703b3fc52920d354b4e2b06447dba0a5404
Support DatActionS <support@datactions.com>
2020-09-19 23:59:11 +0100

    Basic stuff

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import "./Header.scss";
+import backgroundImage from "./images/background.jpg";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundPosition: "center",
+        backgroundRepeat: false,
+        backgroundSize: "cover",
+      }}
+      className="Header"
+    >
+      <div
+        className="HeaderText"
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            minHeight: "4rem",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h1 style={{ textAlign: "center", color: "white", fontWeight: 500 }}>
+            Cuay's Little Garden
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
