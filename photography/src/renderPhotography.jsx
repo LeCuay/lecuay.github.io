08f907116fd6da12af7b394142350938aca266bb
Support DatActionS <support@datactions.com>
2020-09-19 23:28:25 +0100

    First commit

--- photography/src/renderPhotography.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import CuayNav from "./components/CuayNav";
-
-const App = () => {
-  return <CuayNav />;
-};
-
-ReactDOM.render(<App />, document.getElementById("photography-app"));
