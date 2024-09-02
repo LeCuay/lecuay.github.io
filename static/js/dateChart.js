75c8103030e2f05b76ec6a397c93aa561e617186
LeCuay <suso.becerra98@gmail.com>
2024-09-02 11:25:17 +0000

    feat: styles for smartphones

--- static/js/dateChart.js
+++ static/js/dateChart.js
@@ -11,6 +11,34 @@ function drawChart() {
     document.getElementById("calendar_basic")
   );
 
+  /**
+   *
+   * @returns {{cells: number, yearLabelFontSize: number, dayOfWeekLabel: number}}
+   */
+  const getSizesByScreenSize = () => {
+    const sizes = {
+      cells: 6,
+      yearLabelFontSize: 16,
+      dayOfWeekLabel: 6,
+    };
+
+    if (window.innerWidth > 768) {
+      sizes.cells = 12;
+      sizes.yearLabelFontSize = 24;
+      sizes.dayOfWeekLabel = 8;
+    }
+
+    if (window.innerWidth > 992) {
+      sizes.cells = 16;
+      sizes.yearLabelFontSize = 32;
+      sizes.dayOfWeekLabel = 12;
+    }
+
+    return sizes;
+  };
+
+  const sizes = getSizesByScreenSize();
+
   var options = {
     title: "¿Hubo rol?",
     height: 350,
@@ -18,16 +46,16 @@ function drawChart() {
     calendar: {
       underYearSpace: 10,
       yearLabel: {
-        fontName: 'Sans-serif',
-        fontSize: 32,
-        color: 'black',
+        fontName: "Sans-serif",
+        fontSize: sizes.yearLabelFontSize,
+        color: "black",
         bold: true,
-        italic: true
+        italic: true,
       },
-      cellSize: window.innerWidth > 960 ? 16 : 12,
+      cellSize: sizes.cells,
       dayOfWeekLabel: {
         fontName: "Sans-serif",
-        fontSize: 12,
+        fontSize: sizes.dayOfWeekLabel,
         color: "#1a8763",
         bold: true,
         italic: false,
